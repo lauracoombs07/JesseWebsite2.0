18d67842c83e884f278e93254c91109449396acb
Laura <laura.coombs07@gmail.com>
2018-12-25 23:12:44 -0700

    mobile responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ import "./Home.css";
 class Home extends Component {
   render() {
     return (
+      
       <div class="container1">
+      
         <div class="row">
           <div class="col">
             <img
@@ -13,8 +15,8 @@ class Home extends Component {
               alt="Jesse Profile"
             />
           </div>
-          <div class="col-6">
-            <p class="text-">
+          <div class="col-6 wrapper" >
+            <p class="welcome">
               Welcome! I am an Assistant Professor of Applied Linguistics at
               Northern Arizona University. As a researcher, I specialize in the
               use of corpus linguistic methods to explore patterns of language
@@ -25,7 +27,7 @@ class Home extends Component {
               statistical techniques to language data.
             </p>
           </div>
-          <div class="col" align-content="center">  
+          <div class="col" style={{ textAlign: 'center'}}>  
            <h6><b>Contact Information</b></h6>
               <h6>BAA 315</h6> 
               <h6>Northern Arizona University</h6>
@@ -41,3 +43,4 @@ class Home extends Component {
 }
 
 export default Home;
+// style={{ textAlign: 'center'}}
